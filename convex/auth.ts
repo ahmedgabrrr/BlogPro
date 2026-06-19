b0d94b8a179286634f9abb53cbbae82b3e45e05f
ahmedgabrrr <agabr6532@gmail.com>
2026-06-19 20:08:23 +0300

    small change

--- convex/auth.ts
+++ convex/auth.ts
@@ -6,30 +6,32 @@ import { query } from "./_generated/server";
 import { betterAuth } from "better-auth/minimal";
 import authConfig from "./auth.config";
 
-const siteUrl = process.env.SITE_URL!;
+const siteUrl = process.env.SITE_URL || process.env.CONVEX_SITE_URL;
 
-// The component client has methods needed for integrating Convex with Better Auth,
-// as well as helper methods for general use.
 export const authComponent = createClient<DataModel>(components.betterAuth);
 
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
+  if (!siteUrl) {
+    console.error("🚨 Warning: SITE_URL or CONVEX_SITE_URL is not defined in Convex Environment Variables!");
+  }
+
   return betterAuth({
     baseURL: siteUrl,
     database: authComponent.adapter(ctx),
-    // Configure simple, non-verified email/password to get started
+
     emailAndPassword: {
       enabled: true,
       requireEmailVerification: false,
     },
     plugins: [
-      // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
     ],
+    trustedOrigins: [
+      "https://blog-pro-x7zh.vercel.app"
+    ]
   })
 }
 
-// Example function for getting the current user
-// Feel free to edit, omit, etc.
 export const getCurrentUser = query({
   args: {},
   handler: async (ctx) => {
